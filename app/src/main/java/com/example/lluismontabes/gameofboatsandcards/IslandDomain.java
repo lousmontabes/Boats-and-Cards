ddd7aba99e46bd23937c049b8416b09811439481
lousmontabes <lluis.456@gmail.com>
2017-03-13 12:55:48 +0100

    Canvis de noms de variables

--- app/src/main/java/com/example/lluismontabes/gameofboatsandcards/IslandDomain.java
+++ app/src/main/java/com/example/lluismontabes/gameofboatsandcards/IslandDomain.java
@@ -12,8 +12,7 @@ import android.view.View;
 
 class IslandDomain extends View {
 
-
-    Paint radIsland;
+    Paint islandArea;
 
     public IslandDomain(Context context) {
         super(context);
@@ -21,17 +20,17 @@ class IslandDomain extends View {
     }
 
     private Paint styleDefine(){
-        this.radIsland = new Paint(Paint.ANTI_ALIAS_FLAG);
-        radIsland.setColor(Color.GREEN);
-        radIsland.setStyle(Paint.Style.FILL_AND_STROKE);
-        return radIsland;
+        this.islandArea = new Paint(Paint.ANTI_ALIAS_FLAG);
+        islandArea.setColor(Color.GREEN);
+        islandArea.setStyle(Paint.Style.FILL_AND_STROKE);
+        return islandArea;
     }
 
     @Override
     protected void onDraw(Canvas canvas){
-        int alt = canvas.getHeight();
-        int anc = canvas.getWidth();
-        canvas.drawCircle(anc/2,alt/2,50,radIsland);
+        int canvasHeight = canvas.getHeight();
+        int canvasWidth = canvas.getWidth();
+        canvas.drawCircle(canvasWidth / 2, canvasHeight / 2, 300, islandArea);
 
     }
 
