89c07cb8bdb6c117bfe9a574a5cede16344fdb7d
olekboad13.alumnes <osleboa@gmail.com>
2017-03-20 10:59:09 +0100

    Funcionalitats "Online Match" i "Exit" implementades

--- app/src/main/java/com/example/lluismontabes/gameofboatsandcards/MainMenuActivity.java
+++ app/src/main/java/com/example/lluismontabes/gameofboatsandcards/MainMenuActivity.java
@@ -1,15 +1,17 @@
 package com.example.lluismontabes.gameofboatsandcards;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.StringRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.Toast;
 
 
 public class MainMenuActivity extends AppCompatActivity {
@@ -21,6 +23,7 @@ public class MainMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         Button buto = (Button) findViewById(R.id.buttonSP);
+        final Context c = this.getApplicationContext();
         buto.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v) {
@@ -32,8 +35,48 @@ public class MainMenuActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        buto = (Button) findViewById(R.id.buttonMP);
+        buto.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                Toast.makeText(c, getString(R.string.comingSoon), Toast.LENGTH_SHORT).show();
+            }
+        });
+        buto = (Button) findViewById(R.id.buttonSet);
+        buto.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                // create an Intent using the current Activity
+                // and the Class to be created
+                Intent i = new Intent(MainMenuActivity.this,MainActivity.class);
+                // pass the Intent to the Activity,
+                // using the specified action defined in StartPage
+                startActivity(i);
+            }
+        });
+        buto = (Button) findViewById(R.id.buttonCred);
+        buto.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                // create an Intent using the current Activity
+                // and the Class to be created
+                Intent i = new Intent(MainMenuActivity.this,MainActivity.class);
+                // pass the Intent to the Activity,
+                // using the specified action defined in StartPage
+                startActivity(i);
+            }
+        });
+        buto = (Button) findViewById(R.id.buttonExit);
+        buto.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                MainMenuActivity.this.finish();
+            }
+        });
     }
 
-
-
 }
+
+
+
+
