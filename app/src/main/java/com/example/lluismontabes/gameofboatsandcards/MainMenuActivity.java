f1122be64cf8e3a0f47b255af7ccdd5affc7c4af
olekboad13.alumnes <osleboa@gmail.com>
2017-03-20 10:33:17 +0100

    Activities unides

--- app/src/main/java/com/example/lluismontabes/gameofboatsandcards/MainMenuActivity.java
+++ app/src/main/java/com/example/lluismontabes/gameofboatsandcards/MainMenuActivity.java
@@ -1,5 +1,6 @@
 package com.example.lluismontabes.gameofboatsandcards;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -19,9 +20,19 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-
+        Button buto = (Button) findViewById(R.id.buttonSP);
+        buto.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                // create an Intent using the current Activity
+                // and the Class to be created
+                Intent i = new Intent(MainMenuActivity.this,MainActivity.class);
+                // pass the Intent to the Activity,
+                // using the specified action defined in StartPage
+                startActivity(i);
+            }
+        });
     }
-    Button buto = (Button) findViewById(R.id.buttonSP);
 
 
 
