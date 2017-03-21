c3ff45666f58c0e475d5c46fe5ea983ddbc111d0
olekboad13.alumnes <osleboa@gmail.com>
2017-03-21 12:49:10 +0100

    Posat contingut als crèdits

--- app/src/main/java/com/example/lluismontabes/gameofboatsandcards/CreditsActivity.java
+++ app/src/main/java/com/example/lluismontabes/gameofboatsandcards/CreditsActivity.java
@@ -2,12 +2,23 @@ package com.example.lluismontabes.gameofboatsandcards;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class CreditsActivity extends AppCompatActivity {
+
+    Timer refreshTimer;
+    private long refreshPeriod = 1000 / 60; // Period in milliseconds of each update
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
     }
+
+
 }
