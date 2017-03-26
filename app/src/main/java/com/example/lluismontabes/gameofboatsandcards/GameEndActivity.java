9cb610a6943fe91ea55c227bcfbf3a5b0a0c718d
olb98 <osleboa@gmail.com>
2017-03-26 20:16:52 +0200

    Afegida una GameEndActivity pel final de cada partida

---
+++ app/src/main/java/com/example/lluismontabes/gameofboatsandcards/GameEndActivity.java
@@ -0,0 +1,19 @@
+package com.example.lluismontabes.gameofboatsandcards;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class GameEndActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_end);
+
+    }
+
+}
