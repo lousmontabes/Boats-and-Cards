9c3ace7c60ddf7118adc7f3b4f881a54c35991a7
olb98 <osleboa@gmail.com>
2017-03-27 23:23:43 +0200

    Afegida classe CardView (de moment buida)

---
+++ app/src/main/java/com/example/lluismontabes/gameofboatsandcards/CardView.java
@@ -0,0 +1,15 @@
+package com.example.lluismontabes.gameofboatsandcards;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+/**
+ * Created by Delta on 27/03/2017.
+ */
+
+public class CardView extends ImageView {
+
+    public CardView(Context context) {
+        super(context);
+    }
+}
