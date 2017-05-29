a0c73784fcb7d797f61d59e70ecf4a49eb2f76c8
olekboad13.alumnes <osleboa@gmail.com>
2017-05-29 11:06:56 +0200

    Die MainMenuActivity ist fertiggestellt. (trad: la MainMenuActivity està completada)

--- app/src/main/java/com/example/lluismontabes/gameofboatsandcards/Interface/MainMenuActivity.java
+++ app/src/main/java/com/example/lluismontabes/gameofboatsandcards/Interface/MainMenuActivity.java
@@ -1,6 +1,7 @@
 package com.example.lluismontabes.gameofboatsandcards.Interface;
 
 import android.content.Intent;
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,11 +9,15 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.lluismontabes.gameofboatsandcards.R;
 
+
 public class MainMenuActivity extends AppCompatActivity {
+
+    private static boolean soundActive = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +29,10 @@ public class MainMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         Button playButton = (Button) findViewById(R.id.playButton);
-        TextView settingsTextView = (TextView) findViewById(R.id.settingsTextView);
+        //TextView settingsTextView = (TextView) findViewById(R.id.collectionTextView);
         //Button debugButton = (Button) findViewById(R.id.button3);
-        //Button collectionButton = (Button) findViewById(R.id.collectionButton);
+        TextView collectionTextView = (TextView) findViewById(R.id.collectionTextView);
+        ImageButton soundButton = (ImageButton) findViewById(R.id.soundButton);
 
         playButton.setOnClickListener(new View.OnClickListener()
         {
@@ -39,7 +45,7 @@ public class MainMenuActivity extends AppCompatActivity {
             }
         });
 
-        settingsTextView.setOnClickListener(new View.OnClickListener()
+        /*settingsTextView.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v) {
 
@@ -50,7 +56,7 @@ public class MainMenuActivity extends AppCompatActivity {
             }
         });
 
-        /*debugButton.setOnClickListener(new View.OnClickListener()
+        debugButton.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v) {
 
@@ -60,9 +66,9 @@ public class MainMenuActivity extends AppCompatActivity {
 
 
             }
-        });*
+        });*/
 
-        collectionButton.setOnClickListener(new View.OnClickListener() {
+        collectionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -71,12 +77,23 @@ public class MainMenuActivity extends AppCompatActivity {
                 startActivity(i);
 
             }
-        });*/
+        });
 
-        MediaPlayer mp;
+        final MediaPlayer mp;
         mp = MediaPlayer.create(getApplicationContext(), R.raw.main_menu_audio);
         mp.setLooping(true);
         mp.start();
+
+        soundButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(soundActive ^= true) {
+                    mp.start();
+                } else {
+                    mp.stop();
+                }
+            }
+        });
 
     }
 
